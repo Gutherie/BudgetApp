b55556182272cbdefd383f13779fdb372b836e99
jabaker <jabaker@research13.mcmaster.ca>
2015-10-09 15:25:16 -0400

    Added the session object

--- budgetApp/src/main/java/com/gutherie/budgetApp/controller/SessionData.java
+++ budgetApp/src/main/java/com/gutherie/budgetApp/controller/SessionData.java
@@ -4,9 +4,10 @@ import java.util.Locale;
 
 import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 @Component
-@Scope("session")
+@Scope(value="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
 
 public class SessionData {
 	public void setLocal(Locale aLocale){
